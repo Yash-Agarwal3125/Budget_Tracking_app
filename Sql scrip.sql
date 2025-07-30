eaf08bfee95bf23754656f900a8a63cc56acb8d3
Yash Agarwal <yashagarwal3125@gmail.com>
2025-07-30 22:23:07 +0530

    SQL script created

---
+++ Sql scrip.sql
@@ -0,0 +1,21 @@
+create table User(
+    user_id int primary key,
+    user_name varchar(50) not null,
+    email varchar(100) not null unique,
+    password varchar(255) not null
+);
+
+
+create table transaction(
+    transaction_id int primary key,
+    user_id int ,
+    type enum('Income', 'Expense', 'Payable', 'Receivable') not null,
+    amount decimal(10, 2) not null,
+    category_name varchar(50) not null,
+    description text,
+    date timestamp not null default current_timestamp,
+    person_involved varchar(100),
+    status enum('Pending', 'Paid', 'Completed') not null,
+    foreign key (user_id)references User(user_id)
+);    
+
